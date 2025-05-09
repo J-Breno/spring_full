545f414e4524fca983014f2504a349c8897ddce2
J-Breno <joao.breno85@hotmail.com>
2025-05-09 20:34:32 -0300

    criando a estrutura do projeto

---
+++ 02-project-gestão-de-vagas/gestao_vagas/src/main/java/com/github/jbreno/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,17 @@
+package com.github.jbreno.gestao_vagas.modules.candidate;
+
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+    private UUID id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private String description;
+    private String curriculum;
+}
---
+++ 02-project-gestão-de-vagas/gestao_vagas/src/main/java/com/github/jbreno/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,17 @@
+package com.github.jbreno.gestao_vagas.modules.candidate.controllers;
+
+import com.github.jbreno.gestao_vagas.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidate) {
+        System.out.println(candidate.getEmail());
+    }
+}
