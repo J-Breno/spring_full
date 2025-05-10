217e857292fb81b6f714f3bd6743249d73dbfab0
J-Breno <joao.breno85@hotmail.com>
2025-05-09 21:00:45 -0300

    Formatando mensagem de valição

--- 02-project-gestão-de-vagas/gestao_vagas/src/main/java/com/github/jbreno/gestao_vagas/modules/candidate/CandidateEntity.java
+++ 02-project-gestão-de-vagas/gestao_vagas/src/main/java/com/github/jbreno/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,10 @@
 package com.github.jbreno.gestao_vagas.modules.candidate;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.UUID;
@@ -9,8 +13,12 @@ import java.util.UUID;
 public class CandidateEntity {
     private UUID id;
     private String name;
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
+    @Size(min = 10, max = 100, message = "A senha deve conter entre 10 a 100 caracteres")
     private String password;
+    @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
     private String description;
     private String curriculum;
